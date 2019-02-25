1a64a4a839804d438d2e1b1f6ce92dd9660eb2dc
Yang Zhihao <yangzhihao@quannengzhanggui.cn>
2019-02-25 15:31:00 +0800

    config update

---
+++ .postcssrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "plugins": {
+        "autoprefixer": {
+            "browsers": [
+                "iOS >= 7",
+                "Android >= 4.1"
+            ]
+        }
+    }
+}
